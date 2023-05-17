3089f899e98c0a04a7166c954e17a45b6722e27d
Emiliano <emiliano.sangoi@gmail.com>
2023-05-17 13:20:29 -0300

    Creacion de header.php y footer.php y implemento la carga de scripts y css en functions.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Agency - Start Bootstrap Theme</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="<?php echo RPT_THEME_DIR . '/assets/favicon.ico'; ?>" />
+
+    <?php
+        wp_head();
+    ?>
+</head>
+<body id="page-top">
