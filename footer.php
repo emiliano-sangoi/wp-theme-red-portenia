2f77e69c5e40ad16f5dd399e77afc33c45861170
Emiliano <emiliano.sangoi@gmail.com>
2023-05-23 21:32:42 -0300

    Correccion de div mal cerrada en footer.

--- footer.php
+++ footer.php
@@ -1,46 +1,41 @@
 <!-- Footer-->
 <footer class="footer">
-
-    <div class="bg-secondary">
-        <div class="row text-left">
-            <div class="col-lg-8 py-3">
-                <h2 class="section-heading text-white">OFICINAS</h2>
-                <div class="row">
-                    <div class="col-lg-4 col-sm-6 mb-4">
-                        <h4 class="text-white ">COMERCIAL</h4>
-                        <p class="lead mb-0 text-white ">
-                            Av. J. Dgo. Peron 2101 (PAER)
-                            </br>
-                            (2300) Rafaela, Santa Fe.
-                            </br>
-                            comercial2@redportena.com.ar
-                            </br>
-                            03492 - 15650096 / 5707321
-                        </p>
-                    </div>
-
-                    <div class="col-lg-4 col-sm-6 mb-4">
-                        <h5 class="text-white">ADMINISTRATIVA</h5>
-                        <p class="lead mb-0 text-white">
-                            Av. J. Dgo. Peron 2101 (PAER)
-                            </br>
-                            (2300) Rafaela, Santa Fe.
-                            </br>
-                            facturacion@redportena.com.ar
-                            </br>
-                            03492 - 15650098 / 15338122
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 py-3">
-                <h2 class="section-heading text-white">REDES</h2>
-            </div>
-        </div>
-    </div>
-    </div>
-
+<!--    <div class="bg-secondary">-->
+<!--        <div class="row text-left">-->
+<!--            <div class="col-lg-8 py-3">-->
+<!--                <h2 class="section-heading text-white">OFICINAS</h2>-->
+<!--                <div class="row">-->
+<!--                    <div class="col-lg-4 col-sm-6 mb-4">-->
+<!--                        <h4 class="text-white ">COMERCIAL</h4>-->
+<!--                        <p class="lead mb-0 text-white ">-->
+<!--                            Av. J. Dgo. Peron 2101 (PAER)-->
+<!--                            </br>-->
+<!--                            (2300) Rafaela, Santa Fe.-->
+<!--                            </br>-->
+<!--                            comercial2@redportena.com.ar-->
+<!--                            </br>-->
+<!--                            03492 - 15650096 / 5707321-->
+<!--                        </p>-->
+<!--                    </div>-->
+<!--                    <div class="col-lg-4 col-sm-6 mb-4">-->
+<!--                        <h5 class="text-white">ADMINISTRATIVA</h5>-->
+<!--                        <p class="lead mb-0 text-white">-->
+<!--                            Av. J. Dgo. Peron 2101 (PAER)-->
+<!--                            </br>-->
+<!--                            (2300) Rafaela, Santa Fe.-->
+<!--                            </br>-->
+<!--                            facturacion@redportena.com.ar-->
+<!--                            </br>-->
+<!--                            03492 - 15650098 / 15338122-->
+<!--                        </p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="col-lg-4 py-3">-->
+<!--                <h2 class="section-heading text-white">REDES</h2>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
     <div class="container py-4">
         <div class="row align-items-center">
             <div class="col-lg-6 text-lg-start">Copyright &copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>>
@@ -55,8 +50,6 @@
             </div>
         </div>
     </div>
-    </div>
-
 </footer>
 <!-- Portfolio Modals-->
 <div class="portfolio-modal modal fade" id="faqSociosModal" tabindex="-1" role="dialog" aria-hidden="true">
@@ -116,246 +109,6 @@
 
 
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Portfolio item 1 modal popup-->
-<div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="close-modal" data-bs-dismiss="modal"><img
-                    src="<?php echo RPT_THEME_DIR . '/assets/img/close-icon.svg'; ?>" alt="Close modal"/></div>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8">
-                        <div class="modal-body">
-                            <!-- Project details-->
-                            <h2 class="text-uppercase">Project Name</h2>
-                            <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                            <img class="img-fluid d-block mx-auto"
-                                 src="<?php echo RPT_THEME_DIR . '/assets/img/portfolio/1.jpg'; ?>" alt="..."/>
-                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt
-                                repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae,
-                                nostrum, reiciendis facere nemo!</p>
-                            <ul class="list-inline">
-                                <li>
-                                    <strong>Client:</strong>
-                                    Threads
-                                </li>
-                                <li>
-                                    <strong>Category:</strong>
-                                    Illustration
-                                </li>
-                            </ul>
-                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                <i class="fas fa-xmark me-1"></i>
-                                Close Project
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Portfolio item 2 modal popup-->
-<div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="close-modal" data-bs-dismiss="modal"><img
-                    src="<?php echo RPT_THEME_DIR . '/assets/img/close-icon.svg'; ?>" alt="Close modal"/></div>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8">
-                        <div class="modal-body">
-                            <!-- Project details-->
-                            <h2 class="text-uppercase">Project Name</h2>
-                            <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                            <img class="img-fluid d-block mx-auto"
-                                 src="<?php echo RPT_THEME_DIR . '/assets/img/portfolio/2.jpg'; ?>" alt="..."/>
-                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt
-                                repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae,
-                                nostrum, reiciendis facere nemo!</p>
-                            <ul class="list-inline">
-                                <li>
-                                    <strong>Client:</strong>
-                                    Explore
-                                </li>
-                                <li>
-                                    <strong>Category:</strong>
-                                    Graphic Design
-                                </li>
-                            </ul>
-                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                <i class="fas fa-xmark me-1"></i>
-                                Close Project
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Portfolio item 3 modal popup-->
-<div class="modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="close-modal" data-bs-dismiss="modal"><img
-                    src="<?php echo RPT_THEME_DIR . '/assets/img/close-icon.svg'; ?>" alt="Close modal"/></div>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8">
-                        <div class="modal-body">
-                            <!-- Project details-->
-                            <h2 class="text-uppercase">Project Name</h2>
-                            <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                            <img class="img-fluid d-block mx-auto"
-                                 src="<?php echo RPT_THEME_DIR . '/assets/img/portfolio/3.jpg'; ?>" alt="..."/>
-                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt
-                                repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae,
-                                nostrum, reiciendis facere nemo!</p>
-                            <ul class="list-inline">
-                                <li>
-                                    <strong>Client:</strong>
-                                    Finish
-                                </li>
-                                <li>
-                                    <strong>Category:</strong>
-                                    Identity
-                                </li>
-                            </ul>
-                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                <i class="fas fa-xmark me-1"></i>
-                                Close Project
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Portfolio item 4 modal popup-->
-<div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="close-modal" data-bs-dismiss="modal"><img
-                    src="<?php echo RPT_THEME_DIR . '/assets/img/close-icon.svg'; ?>" alt="Close modal"/></div>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8">
-                        <div class="modal-body">
-                            <!-- Project details-->
-                            <h2 class="text-uppercase">Project Name</h2>
-                            <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                            <img class="img-fluid d-block mx-auto"
-                                 src="<?php echo RPT_THEME_DIR . '/assets/img/portfolio/4.jpg'; ?>" alt="..."/>
-                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt
-                                repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae,
-                                nostrum, reiciendis facere nemo!</p>
-                            <ul class="list-inline">
-                                <li>
-                                    <strong>Client:</strong>
-                                    Lines
-                                </li>
-                                <li>
-                                    <strong>Category:</strong>
-                                    Branding
-                                </li>
-                            </ul>
-                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                <i class="fas fa-xmark me-1"></i>
-                                Close Project
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Portfolio item 5 modal popup-->
-<div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="close-modal" data-bs-dismiss="modal"><img
-                    src="<?php echo RPT_THEME_DIR . '/assets/img/close-icon.svg'; ?>" alt="Close modal"/></div>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8">
-                        <div class="modal-body">
-                            <!-- Project details-->
-                            <h2 class="text-uppercase">Project Name</h2>
-                            <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                            <img class="img-fluid d-block mx-auto"
-                                 src="<?php echo RPT_THEME_DIR . '/assets/img/portfolio/5.jpg'; ?>" alt="..."/>
-                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt
-                                repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae,
-                                nostrum, reiciendis facere nemo!</p>
-                            <ul class="list-inline">
-                                <li>
-                                    <strong>Client:</strong>
-                                    Southwest
-                                </li>
-                                <li>
-                                    <strong>Category:</strong>
-                                    Website Design
-                                </li>
-                            </ul>
-                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                <i class="fas fa-xmark me-1"></i>
-                                Close Project
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Portfolio item 6 modal popup-->
-<div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="close-modal" data-bs-dismiss="modal"><img
-                    src="<?php echo RPT_THEME_DIR . '/assets/img/close-icon.svg'; ?>" alt="Close modal"/></div>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8">
-                        <div class="modal-body">
-                            <!-- Project details-->
-                            <h2 class="text-uppercase">Project Name</h2>
-                            <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                            <img class="img-fluid d-block mx-auto"
-                                 src="<?php echo RPT_THEME_DIR . '/assets/img/portfolio/6.jpg'; ?>" alt="..."/>
-                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt
-                                repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae,
-                                nostrum, reiciendis facere nemo!</p>
-                            <ul class="list-inline">
-                                <li>
-                                    <strong>Client:</strong>
-                                    Window
-                                </li>
-                                <li>
-                                    <strong>Category:</strong>
-                                    Photography
-                                </li>
-                            </ul>
-                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                <i class="fas fa-xmark me-1"></i>
-                                Close Project
-                            </button>
                         </div>
                     </div>
                 </div>
